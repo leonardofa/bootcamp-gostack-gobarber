9aab4e8dce202529e8db5fcbc5bca99df9fff01f
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-02-05 13:48:22 -0300

    Listando agenda do prestador

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -58,10 +58,14 @@ class AppointmentController {
   }
 
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.idUser, canceled_at: null },
       order: [['date', 'desc']],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
