c55c8cea1f37d4938286de8be9cb1fc6ba6db4fc
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-02-05 22:07:42 -0300

    Marcar notificações como lidas

--- src/routes.js
+++ src/routes.js
@@ -33,6 +33,7 @@ routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
