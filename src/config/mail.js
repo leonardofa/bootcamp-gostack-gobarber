eabd0c78deadf46ba6526cab87e6bea6e41af744
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-02-09 21:01:26 -0300

    Configurando fila com Redis

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'localhost',
-  port: 1025,
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: '',
-    pass: '',
+    user: 'f84e09650008c4',
+    pass: 'dc72c6073ebe52',
   },
   secure: false,
   default: {
--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,5 +1,34 @@
 import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
 
-class Queue {}
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, { redis: { redisConfig } }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+      bee.process(handle);
+    });
+  }
+}
 
 export default new Queue();
